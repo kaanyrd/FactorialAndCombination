5e5ca065bf266e5b78a687b2e16090a25154cf5e
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2022-09-10 20:47:49 +0300

    FaktoriyelOKCombinationNON

--- app.js
+++ app.js
@@ -16,20 +16,24 @@ if(number > 0){
     addResult(`${factorial.value} => SAYISININ FAKTÖRİYEL SONUCU = ${input}`)
     console.log(`${factorial.value} => SAYISININ FAKTÖRİYEL SONUCU = ${input}`)
     factorial.value = ""
-    
+
 }
 else if(number === 0){
     addResult(`0 => SAYISININ FAKTÖRİYEL SONUCU = ${1}`)
     factorial.value=""
     console.log(`0 SAYISININ FAKTÖRİYEL SONUCU BULUNDU: ${1}`)
+
+
+
+    
 }
 else{
     let alertDOM = document.querySelector("#alert")
     let alert = document.createElement("h6")
     alert.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <strong>HATALI GİRİŞ!</strong> Lütfen pozitif bir değer giriniz...
+    <strong>HATALI GİRİŞ!</strong> Faktöriyelini hesaplamak için geçerli sayılar giriniz... <strong>(SIFIR VEYA POZİTİF TAMSAYILAR)<strong> 
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
+    <span aria-hidden="true">&times;</span>
     </button>
   </div>`
     alert.classList.add("alert-danger", "pl-3")
@@ -37,7 +41,7 @@ else{
     alertDOM.append(alert)
 }
 }
-
+// VERİ EKLEME
 let factoListDOM = document.querySelector("#factoList")
 
 let addResult = (facto) => {
@@ -46,4 +50,16 @@ liDOM.innerHTML = `${facto}`
 liDOM.classList.add("list-group-item", "text-danger")
 liDOM.style["background-color"]="#d5e1df";
 factoListDOM.append(liDOM)
+}
+
+// VERİ SİLME
+let deleteButton = document.querySelector("#factoDelete")
+deleteButton.addEventListener("click", deleteItem)
+
+function deleteItem(silmeventi){
+silmeventi.preventDefault()
+console.log("işlem gerçekleştirildi")
+
+factoListDOM.removeChild(factoListDOM.lastChild)
+
 }
